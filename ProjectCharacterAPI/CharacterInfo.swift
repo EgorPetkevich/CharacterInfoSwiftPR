6d1a421089ec0c4e96c09b99c543c8a750469ee5
EgorPetkevich <egorpetkevich01@gmail.com>
2023-10-12 20:21:49 +0300

    final commit from EgorPetkevich branch

---
+++ ProjectCharacterAPI/CharacterInfo.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterInfo.swift
+//  ProjectCharacterAPI
+//
+//  Created by George Popkich on 12.10.23.
+//
+
+import Foundation
---
+++ ProjectCharacterAPI/CollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  CollectionViewCell.swift
+//  ProjectCharacterAPI
+//
+//  Created by George Popkich on 12.10.23.
+//
+
+import Foundation
